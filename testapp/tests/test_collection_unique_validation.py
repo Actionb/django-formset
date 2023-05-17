9f037f0174f42e7732e1e3625c07a46f8d041a63
Actionb <yummytea1@gmail.com>
2023-05-17 13:05:02 +0200

    Rewrite tests as pytest tests

--- testapp/tests/test_collection_unique_validation.py
+++ testapp/tests/test_collection_unique_validation.py
@@ -1,100 +1,118 @@
-from django.test import TestCase
+import pytest
 
 from testapp.forms.company import CompanyCollection
 from testapp.models import Company, Team, Member
 
 
-class TestUniqueValidation(TestCase):
+@pytest.fixture
+def team(company):
+    return Team.objects.create(name="Alice Validation Team", company=company)
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.company = Company.objects.create(name="Foo Corp")
-        cls.team = Team.objects.create(name="Alice Validation Team", company=cls.company)
-        cls.alice = Member.objects.create(name="Alice", team=cls.team)
 
-    def test_control(self):
-        data = {
-            "company": {"name": self.company.name, "id": self.company.pk},
-            "teams": [
-                {
-                    "team": {"name": self.team.name, "id": self.team.pk},
-                    "members": [
-                        {"member": {"name": self.alice.name, "id": self.alice.pk}}
-                    ]
-                },
-            ]
-        }
-        coll = CompanyCollection(data=data, instance=self.company)
-        self.assertTrue(coll.is_valid())
+@pytest.fixture
+def company():
+    return Company.objects.create(name="Foo Corp")
 
-    def test_add_duplicate_team(self):
-        data = {
-            "company": {"name": self.company.name, "id": self.company.pk},
-            "teams": [
-                {
-                    "team": {"name": self.team.name, "id": self.team.pk},
-                    "members": [
-                        {"member": {"name": self.alice.name, "id": self.alice.pk}}
-                    ]
-                },
-                {
-                    "team": {"name": self.team.name, "id": None},
-                    "members": []
-                },
-            ]
-        }
-        coll = CompanyCollection(data=data, instance=self.company)
-        self.assertFalse(coll.is_valid())
-        self.assertIn("Please correct the duplicate data for name and company, which must be unique.", str(coll.errors))
 
-    def test_add_duplicate_member(self):
-        data = {
-            "company": {"name": self.company.name, "id": self.company.pk},
-            "teams": [
-                {
-                    "team": {"name": self.team.name, "id": self.team.pk},
-                    "members": [
-                        {"member": {"name": self.alice.name, "id": self.alice.pk}},
-                        {"member": {"name": self.alice.name, "id": None}},
-                    ]
-                },
-            ]
-        }
-        coll = CompanyCollection(data=data, instance=self.company)
-        self.assertFalse(coll.is_valid())
-        self.assertIn("Please correct the duplicate data for name and team, which must be unique.", str(coll.errors))
+@pytest.fixture
+def member(team):
+    return Member.objects.create(name="Alice", team=team)
 
-    def test_add_new_team_with_duplicate_members(self):
-        data = {
-            "company": {"name": self.company.name, "id": self.company.pk},
-            "teams": [
-                {
-                    "team": {"name": "Bob's Test Team", "id": None},
-                    "members": [
-                        {"member": {"name": "Bob", "id": None}},
-                        {"member": {"name": "Bob", "id": None}},
-                    ]
-                },
-            ]
-        }
-        coll = CompanyCollection(data=data, instance=self.company)
-        self.assertFalse(coll.is_valid())
-        self.assertIn("Please correct the duplicate data for name and team, which must be unique.", str(coll.errors))
 
-    def test_add_new_company_with_duplicate_teams(self):
-        data = {
-            "company": {"name": "Bob Corp", "id": None},
-            "teams": [
-                {
-                    "team": {"name": "Bob's Test Team", "id": None},
-                    "members": []
-                },
-                {
-                    "team": {"name": "Bob's Test Team", "id": None},
-                    "members": []
-                },
-            ]
-        }
-        coll = CompanyCollection(data=data, instance=self.company)
-        self.assertFalse(coll.is_valid())
-        self.assertIn("Please correct the duplicate data for name and company, which must be unique.", str(coll.errors))
+@pytest.mark.django_db
+def test_control(company, team, member):
+    # protect against the possibility that the other tests pass because the
+    # collection is always invalid
+    data = {
+        "company": {"name": company.name, "id": company.pk},
+        "teams": [
+            {
+                "team": {"name": team.name, "id": team.pk},
+                "members": [
+                    {"member": {"name": member.name, "id": member.pk}}
+                ]
+            },
+        ]
+    }
+    coll = CompanyCollection(data=data, instance=company)
+    assert coll.is_valid()
+
+
+@pytest.mark.django_db
+def test_add_duplicate_team(company, team, member):
+    data = {
+        "company": {"name": company.name, "id": company.pk},
+        "teams": [
+            {
+                "team": {"name": team.name, "id": team.pk},
+                "members": [
+                    {"member": {"name": member.name, "id": member.pk}}
+                ]
+            },
+            {
+                "team": {"name": team.name, "id": None},
+                "members": []
+            },
+        ]
+    }
+    coll = CompanyCollection(data=data, instance=company)
+    assert not coll.is_valid()
+    assert "Please correct the duplicate data for name and company, which must be unique." in str(coll.errors)
+
+
+@pytest.mark.django_db
+def test_add_duplicate_member(company, team, member):
+    data = {
+        "company": {"name": company.name, "id": company.pk},
+        "teams": [
+            {
+                "team": {"name": team.name, "id": team.pk},
+                "members": [
+                    {"member": {"name": member.name, "id": member.pk}},
+                    {"member": {"name": member.name, "id": None}},
+                ]
+            },
+        ]
+    }
+    coll = CompanyCollection(data=data, instance=company)
+    assert not coll.is_valid()
+    assert "Please correct the duplicate data for name and team, which must be unique." in str(coll.errors)
+
+
+@pytest.mark.django_db
+def test_add_new_team_with_duplicate_members(company, team, member):
+    data = {
+        "company": {"name": company.name, "id": company.pk},
+        "teams": [
+            {
+                "team": {"name": "Bob's Test Team", "id": None},
+                "members": [
+                    {"member": {"name": "Bob", "id": None}},
+                    {"member": {"name": "Bob", "id": None}},
+                ]
+            },
+        ]
+    }
+    coll = CompanyCollection(data=data, instance=company)
+    assert not coll.is_valid()
+    assert "Please correct the duplicate data for name and team, which must be unique." in str(coll.errors)
+
+
+@pytest.mark.django_db
+def test_add_new_company_with_duplicate_teams(company, team, member):
+    data = {
+        "company": {"name": "Bob Corp", "id": None},
+        "teams": [
+            {
+                "team": {"name": "Bob's Test Team", "id": None},
+                "members": []
+            },
+            {
+                "team": {"name": "Bob's Test Team", "id": None},
+                "members": []
+            },
+        ]
+    }
+    coll = CompanyCollection(data=data, instance=company)
+    assert not coll.is_valid()
+    assert "Please correct the duplicate data for name and company, which must be unique." in str(coll.errors)
